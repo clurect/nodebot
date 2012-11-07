5122391938864592f5a95fa4b6b52695642e915a
Stephen Wright <swright@PFI-C02GJ0K4DRJM.local>
2012-11-07 11:56:57 -0500

    Adding a really cool quote feature.

---
+++ scripts/quote.js
@@ -0,0 +1,101 @@
+var fs = require('fs');
+function getJson(filename,data,func){
+	retval = "";
+	fs.readFile(filename, 'utf8', function (err, cont) {
+	  if (err) {
+	    console.log('Error: ' + err);
+	    return;
+	  }
+
+	  vault = JSON.parse(cont);
+	  //console.log(vault);
+	  func(data,vault);
+	  //processData(data);
+	  //
+	  //console.dir(data);
+	});
+}
+
+function list (replyTo){
+	var filename = "./data/db.json";
+	getJson(filename,"",printout)
+	function printout(data,vault){
+		var quotes = vault["quotes"];
+		for (var i=0;i<quotes.length;i++){
+			irc.action(replyTo, quotes[i]);
+		}
+	}
+}
+
+function flush (){
+	var filename = "./data/db.json";
+	getJson(filename,"",flushit)
+	function flushit(data,vault){
+		var quotes = vault["quotes"];
+		quotes=[];
+		fs.writeFile(filename, JSON.stringify(vault, null, 4), function(err) {
+		    if(err) {
+		      console.log(err);
+		    } else {
+		      console.log("JSON saved to " + filename);
+		    }
+		}); 
+	}
+}
+
+function store (data){
+	
+	var filename = "./data/db.json";
+	getJson(filename, data, processData) ;
+	//console.log(vault);
+	//processData(data);
+
+	// console.log(vault["quotes"]);
+	
+	function processData(data,vault){
+		var quotes = vault["quotes"];
+		quotes.push(data);
+		
+		fs.writeFile(filename, JSON.stringify(vault, null, 4), function(err) {
+		    if(err) {
+		      console.log(err);
+		    } else {
+		      console.log("JSON saved to " + filename);
+		    }
+		}); 
+	}
+};
+function pick(replyTo){
+	//
+	var filename = "./data/db.json";
+	getJson(filename, "", pickq);
+	function pickq(data,vault){
+		
+		var quotes = vault["quotes"];
+		
+		which = Math.floor(Math.random() * quotes.length);
+		console.log("hmm, which one should I send..." + which);
+
+		
+		irc.action(replyTo,quotes[which]);
+	}
+}
+listen(/~quote(\s+)?(.*)$/i, function(match, data, replyTo) {
+    if (match[2].charAt(0)==="-"){
+    	console.log("options to be doing with");
+    	if (match[2].substring(1)==="list"){
+    		list(replyTo);
+    	}
+    	// else if (match[2].substring(1)==="flush"){
+    	// 	flush(replyTo);
+    	// }
+    }else if (match[2].charAt(0)!==""){
+	    //console.log(match[2].charAt(0));
+	   	store(match[2]);
+	    irc.action(replyTo, "Ooo that was a good one.");
+	}
+	else {
+		pick(replyTo);
+	}
+
+}, false, false);
